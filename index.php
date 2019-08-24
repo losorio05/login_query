ae101d59546191cc4d4a7dcc9019d7e21ad53b8b
LUIS OSORIO <desarrollador4@gruporaphael.com>
2019-08-24 17:36:46 -0500

    vamos bien

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 error_reporting(E_ALL);ini_set('display_errors',1);
 include "core/configGeneral.php";
 include "core/session.php";
-include "controlador/vistasControlador.php";$plantilla=new vistasControlador();
+
+include "controlador/vistasControlador.php";
+$plantilla=new vistasControlador();
+
 $plantilla->obtener_plantilla_controlador();
